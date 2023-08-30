56f2798147d300a5f6bf3d73650f8207a904720c
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2023-08-30 12:17:27 +0300

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,40 @@
+// import {hamburger} from '../assets/images/hamburger.svg'
+import { hamburger } from '../assets/icons'
+import {headerLogo} from '../assets/images'
+import { navLinks } from '../constants'
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute w-full z-10 '>
+        <nav className='flex justify-between items-center max-container'>
+            <a href="/">
+                <img 
+                    src={headerLogo}
+                    width={130}
+                    height={49}
+                    />
+            </a>
+
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                {navLinks.map((item) =>(
+                    <li key ={item.label}>
+                        <a href="item.href"
+                            className='font-montserrat leading-normal text-lg text-slate-gray'
+                        >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <div className='hidden max-lg:block'>
+                <img src={hamburger} 
+                alt='hamburger'
+                width={25}
+                height={25} />
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
